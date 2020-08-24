cea3dea194d7a0dabf8128cdb3e10a3048e83c29
andremlsantos <andremlsantos@gmail.com>
2020-08-24 16:23:02 +0200

    Initial commit
    Full app with basics Stack layout

---
+++ HelloWorld/HelloWorld/HelloWorld/GreetPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace HelloWorld
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class GreetPage : ContentPage
+    {
+        public GreetPage()
+        {
+            InitializeComponent();
+
+            //assim e dificil fazer layout com hierarquia
+            //Content = new Label
+            //{
+            //    HorizontalOptions = LayoutOptions.Center,
+            //    VerticalOptions = LayoutOptions.Center,
+            //    Text = "Hello World"
+            //};
+
+            slider.Value = 0.5;
+        }
+
+        //sender - sorce of the event
+        //e - aditional details about event
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            DisplayAlert("Title", "Hello World", "Ok");
+        }
+
+        private void Slider_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            label.Text = String.Format("Value is {0:F2}", e.NewValue);
+        }
+    }
+}
