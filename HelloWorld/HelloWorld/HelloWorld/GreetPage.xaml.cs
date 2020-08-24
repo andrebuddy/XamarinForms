f5efce2a24fa785ddcaba03054c92a0e26ee4cf3
andremlsantos <andremlsantos@gmail.com>
2020-08-24 16:58:57 +0200

    Seção 2:XAML Essentials
    16. Dealing with Device Differences

--- HelloWorld/HelloWorld/HelloWorld/GreetPage.xaml.cs
+++ HelloWorld/HelloWorld/HelloWorld/GreetPage.xaml.cs
@@ -17,13 +17,31 @@ namespace HelloWorld
             InitializeComponent();
 
             slider.Value = 0.5;
-        }
 
-        //sender - sorce of the event
-        //e - aditional details about event
-        private void Button_Clicked(object sender, EventArgs e)
-        {
-            DisplayAlert("Title", "Hello World", "Ok");
+            // Dealing with Device preferences
+            if (Device.OS == TargetPlatform.iOS)
+                Padding = new Thickness(0, 20, 0, 0);
+            else if(Device.OS == TargetPlatform.Android)
+                Padding = new Thickness(10, 20, 0, 0);
+            else if (Device.OS == TargetPlatform.WinPhone)
+                Padding = new Thickness(30, 20, 0, 0);
+
+            // Dealing with Device preferences 2
+            Padding = Device.OnPlatform(
+                iOS: new Thickness(0, 20, 0, 0),
+                Android: new Thickness(10, 20, 0, 0),
+                WinPhone: new Thickness(30, 20, 0, 0));
+
+            // Dealing with Device preferences 3
+            Device.OnPlatform(
+                iOS: () =>
+                {
+                    Padding = new Thickness(0, 20, 0, 0);
+                },
+                Android: ()=> {
+                    // some code here
+                });
+
         }
     }
 }
