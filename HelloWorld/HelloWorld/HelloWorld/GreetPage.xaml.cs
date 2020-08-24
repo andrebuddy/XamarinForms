a409c085a033d91825f0e0dde431d283f8925dc4
andremlsantos <andremlsantos@gmail.com>
2020-08-24 17:09:52 +0200

    Seção 2:XAML Essentials
    17. Property Element Syntax no XAML

--- HelloWorld/HelloWorld/HelloWorld/GreetPage.xaml.cs
+++ HelloWorld/HelloWorld/HelloWorld/GreetPage.xaml.cs
@@ -17,31 +17,6 @@ namespace HelloWorld
             InitializeComponent();
 
             slider.Value = 0.5;
-
-            // Dealing with Device preferences
-            if (Device.OS == TargetPlatform.iOS)
-                Padding = new Thickness(0, 20, 0, 0);
-            else if(Device.OS == TargetPlatform.Android)
-                Padding = new Thickness(10, 20, 0, 0);
-            else if (Device.OS == TargetPlatform.WinPhone)
-                Padding = new Thickness(30, 20, 0, 0);
-
-            // Dealing with Device preferences 2
-            Padding = Device.OnPlatform(
-                iOS: new Thickness(0, 20, 0, 0),
-                Android: new Thickness(10, 20, 0, 0),
-                WinPhone: new Thickness(30, 20, 0, 0));
-
-            // Dealing with Device preferences 3
-            Device.OnPlatform(
-                iOS: () =>
-                {
-                    Padding = new Thickness(0, 20, 0, 0);
-                },
-                Android: ()=> {
-                    // some code here
-                });
-
         }
     }
 }
