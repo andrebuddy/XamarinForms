2fc43a7f496bdcb57cf88ed23c6c6a84b29f6e5b
andremlsantos <andremlsantos@gmail.com>
2020-08-31 12:20:43 +0200

    Seção 6:Navigation
    61. A Simple Master Detail

---
+++ HelloWorld/HelloWorld/HelloWorld/ContactsPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using HelloWorld.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace HelloWorld
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ContactsPage : ContentPage
+    {
+		public ContactsPage()
+		{
+			InitializeComponent();
+
+			listView.ItemsSource = new List<Contact> {
+				new Contact { Name = "Mosh", ImageUrl = "http://lorempixel.com/100/100/people/1" },
+				new Contact { Name = "John", ImageUrl = "http://lorempixel.com/100/100/people/2", Status = "Hey, let's talk!" }
+			};
+		}
+
+        async private void listView_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+			var contact = e.SelectedItem as Contact;
+
+			if (contact == null)
+				return;
+
+			await Navigation.PushAsync(new ContactDetailsPage(contact));
+			listView.SelectedItem = null;
+        }
+    }
+}
