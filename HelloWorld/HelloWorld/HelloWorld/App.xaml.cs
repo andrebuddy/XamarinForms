8fafeab3ffa4e3b25298ac5c816be85966a9c79b
andremlsantos <andremlsantos@gmail.com>
2020-08-31 13:15:09 +0200

    Seção 6:Navigation
    63. Tabbed Page

--- HelloWorld/HelloWorld/HelloWorld/App.xaml.cs
+++ HelloWorld/HelloWorld/HelloWorld/App.xaml.cs
@@ -16,7 +16,7 @@ namespace HelloWorld
             //    BarTextColor = Color.White
             //};
 
-            MainPage = new ContactsPage();
+            MainPage = new MyTabbedPage();
         }
 
         protected override void OnStart()
