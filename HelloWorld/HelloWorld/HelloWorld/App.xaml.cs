c4ddc1f0a5950ffba93a91639493309a0a9dbf27
DESKTOP-2I3HJLM\andre <a.santos@buddypayment.com>
2020-09-03 13:33:44 +0200

    Seção 7:Forms and Setting Pages
    73. Entry and Editor

--- HelloWorld/HelloWorld/HelloWorld/App.xaml.cs
+++ HelloWorld/HelloWorld/HelloWorld/App.xaml.cs
@@ -16,7 +16,7 @@ namespace HelloWorld
             //    BarTextColor = Color.White
             //};
 
-            MainPage = new NavigationPage(new StepperPage());
+            MainPage = new NavigationPage(new EntryEditorPage());
         }
 
         protected override void OnStart()
---
+++ HelloWorld/HelloWorld/HelloWorld/EntryEditorPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace HelloWorld
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class EntryEditorPage : ContentPage
+    {
+        public EntryEditorPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Entry_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            labelTextChanged.Text = e.NewTextValue;
+        }
+
+        private void Entry_Completed(object sender, EventArgs e)
+        {
+            labelTextCompleted.Text = "Completed";
+        }
+    }
+}
