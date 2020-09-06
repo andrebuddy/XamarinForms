bda938c507eeff2c169ba5ff77ebff416981f20c
andremlsantos <andremlsantos@gmail.com>
2020-09-06 19:46:57 +0200

    Seção 7:Forms and Setting Pages
    79. Picker with Navigation

---
+++ HelloWorld/HelloWorld/HelloWorld/ContactMethodsPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace HelloWorld
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ContactMethodsPage : ContentPage
+    {
+
+        //listview e private entao temos q expor como publico
+        public ListView ContactMethods { get { return listView; } }
+
+        public ContactMethodsPage()
+        {
+            InitializeComponent();
+
+            listView.ItemsSource = new List<string>
+            {
+                "None",
+                "Email",
+                "SMS"
+            };
+        }
+    }
+}
--- HelloWorld/HelloWorld/HelloWorld/TableViewPage.xaml.cs
+++ HelloWorld/HelloWorld/HelloWorld/TableViewPage.xaml.cs
@@ -16,5 +16,17 @@ namespace HelloWorld
         {
             InitializeComponent();
         }
+
+        private void ViewCell_Tapped(object sender, EventArgs e)
+        {
+            var page = new ContactMethodsPage();
+            page.ContactMethods.ItemSelected += (source, args) =>
+            {
+                contactMethod.Text = args.SelectedItem.ToString();
+                Navigation.PopAsync();
+            };
+
+            Navigation.PushAsync(page);
+        }
     }
 }
