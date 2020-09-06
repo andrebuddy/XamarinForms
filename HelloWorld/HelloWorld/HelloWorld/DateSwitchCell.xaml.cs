9b7bcb1328a40b02e43b2cc6bcaf7577221085e5
andremlsantos <andremlsantos@gmail.com>
2020-09-06 18:10:42 +0200

    Seção 7:Forms and Setting Pages
    78. Bindable Properties

---
+++ HelloWorld/HelloWorld/HelloWorld/DateSwitchCell.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace HelloWorld
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class DateSwitchCell : ViewCell
+    {
+        public static readonly BindableProperty LabelProperty =
+            BindableProperty.Create("Label", typeof(string), typeof(DateSwitchCell));
+        public string Label { 
+            get { return (string)GetValue(LabelProperty); } 
+            set { SetValue(LabelProperty, value); } 
+        }
+
+        public DateSwitchCell()
+        {
+            InitializeComponent();
+
+            BindingContext = this;
+        }
+    }
+}
---
+++ HelloWorld/HelloWorld/HelloWorld/DateTimeCell.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace HelloWorld
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class DateTimeCell : ViewCell
+    {
+        public static readonly BindableProperty LabelPropperty = 
+            BindableProperty.Create("Label", typeof(string), typeof(DateTimeCell));
+        public string Label 
+        {
+            get { return (string)GetValue(LabelPropperty); }
+            set { SetValue(LabelPropperty, value); }
+        }
+
+        public DateTimeCell()
+        {
+            InitializeComponent();
+
+            BindingContext = this;
+        }
+    }
+}
