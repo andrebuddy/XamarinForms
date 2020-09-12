669da1027b7625a60ca29576ee5c247def810503
andremlsantos <andremlsantos@gmail.com>
2020-09-12 20:36:39 +0200

    Seção 8:Data Access
    85. A Cleaner Implementation

--- HelloWorld/HelloWorld/HelloWorld/ApplicationProperties.xaml.cs
+++ HelloWorld/HelloWorld/HelloWorld/ApplicationProperties.xaml.cs
@@ -16,33 +16,8 @@ namespace HelloWorld
         {
             InitializeComponent();
 
-            var app = Application.Current as App;
-
-            title.Text = app.Title;
-            notificationsEnabled.On = app.NotificationsEnable;
+            // due to 2 way binding, if we do it like this, the data is auto saved and retrieved
+            BindingContext = Application.Current;
         }
-
-        //standard event handler, 
-        //we can use this for handler of all king events
-        private void OnChanged(object sender, System.EventArgs e)
-        {
-            // Persistence happens when app goes sleep mode
-            // when we open another app, so this one goes background and another foreground
-            // when we quit app
-            var app = Application.Current as App;
-            app.Title = title.Text;
-            app.NotificationsEnable = notificationsEnabled.On;
-
-            // we dont need wait
-            //Application.Current.SavePropertiesAsync();
-        }
-
-        // when we navigate away from this screen
-        protected override void OnDisappearing()
-        {
-            base.OnDisappearing();
-        }
-
-
     }
 }
