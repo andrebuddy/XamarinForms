75dfbfea4f20319dc7b488717885d0983343bcbf
Andre Santos <a.santos@buddypayment.nl>
2021-01-28 23:08:58 +0100

    Secao 9 - Model-View-ViewModel
    105. Exercise

---
+++ ContactBook/ContactBook/ContactBook.Android/Persistence/SQLiteDb.cs
@@ -0,0 +1,23 @@
+﻿using ContactBook.Droid.Persistence;
+using ContactBook.Persistence;
+using SQLite;
+using System;
+using System.IO;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(SQLiteDb))]
+
+
+namespace ContactBook.Droid.Persistence
+{
+    public class SQLiteDb : ISQLiteDb
+    {
+        public SQLiteAsyncConnection GetConnection()
+        {
+            var documentsPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            var path = Path.Combine(documentsPath, "MySQLite.db3");
+
+            return new SQLiteAsyncConnection(path);
+        }
+    }
+}
---
+++ ContactBook/ContactBook/ContactBook/Models/Contact.cs
@@ -0,0 +1,62 @@
+﻿using SQLite;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace ContactBook.Models
+{
+    public class Contact : INotifyPropertyChanged
+    {
+        private string _firstName;
+        private string _lastName;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+
+        [MaxLength(255)]
+        public string FirstName
+        {
+            get { return _firstName; }
+            set
+            {
+                if (_firstName == value)
+                    return;
+
+                _firstName = value;
+
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(FullName));
+            }
+        }
+
+        [MaxLength(255)]
+        public string LastName
+        {
+            get { return _lastName; }
+            set
+            {
+                if (LastName == value)
+                    return;
+
+                _lastName = value;
+
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(FullName));
+            }
+        }
+
+        [MaxLength(255)]
+        public string Phone { get; set; }
+
+        [MaxLength(255)]
+        public string Email { get; set; }
+        public bool IsBlocked { get; set; }
+        public string FullName => $"{FirstName} {LastName}";
+
+        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
