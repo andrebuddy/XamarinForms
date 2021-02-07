71c17682831a9c805d6e8f171b699aa0a9429b19
Andre Santos <a.santos@buddypayment.nl>
2021-02-07 19:35:17 +0100

    Secao-10 - Beyond Basics
    108. Resource Dictionary

---
+++ Resources/Resources/Resources/MainPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using Xamarin.Forms;
+
+namespace Resources
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            // Code behind
+            //this.Resources = new ResourceDictionary();
+            //this.Resources["borderRadius"] = 20;
+        }
+    }
+}
