6d5b4f27b1af90da81af7fd27c5f1fbada3d9714
Andre Santos <a.santos@buddypayment.nl>
2021-02-08 22:34:17 +0100

    Secao 10 - Beyond the Basics
    112 - Messaging Center

---
+++ MessageCenter/MessageCenter/MessageCenter/Pages/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MessageCenter.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnButtonClicked(object sender, EventArgs e)
+        {
+            var page = new TargetPage();
+            page.SliderValueChanged += OnSliderValueChanged;
+
+            Navigation.PushAsync(page);
+        }
+
+        private void OnSliderValueChanged(object source, double newValue)
+        {
+            label.Text = newValue.ToString();
+        }
+    }
+}
---
+++ MessageCenter/MessageCenter/MessageCenter/Pages/TargetPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MessageCenter.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class TargetPage : ContentPage
+    {
+        public event EventHandler<double> SliderValueChanged;
+
+        public TargetPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnSliderChanged(object sender, ValueChangedEventArgs e)
+        {
+            SliderValueChanged?.Invoke(this, e.NewValue);
+        }
+    }
+}
